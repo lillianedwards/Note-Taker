bd42c52b65221457043a40e20fb4b22207f28842
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-09-21 17:18:09 -0500

    added in html routes and api routes

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path");
+const fsUtils = require("./helpers/fsUtils");
+const uuid = require("./helpers/uuid");
+const PORT = process.env.PORT || 3001;
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//html routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+//api routes
+app.get("/api/notes", (req, res) => {
+  fsUtils.readFromFile("./db/db.json")
+  .then((data)=> {
+    // console.log(JSON.parse(data));
+        res.json(JSON.parse(data));
+  })
+});
+
+app.post("/api/notes", (req, res) => {
+  const {title, text} = req.body;
+  const userNote = {
+    title: title,
+    text: text,
+    id: uuid()
+  }
+  fsUtils.readAndAppend(userNote, "./db/db.json")
+  console.log(userNote)
+res.json("Note has been added!✅")
+
+
+});
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
